b5802a2233cfe4ca2db65c8fbd508be7b64ac06a
Daniel Perez <danielperezdeveloper@gmail.com>
2019-12-26 11:33:04 +0100

    starting the project

---
+++ Prueba_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prueba_iOS
+//
+//  Created by Daniel Pérez Parreño on 26/12/2019.
+//  Copyright © 2019 Daniel Pérez Parreño. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
